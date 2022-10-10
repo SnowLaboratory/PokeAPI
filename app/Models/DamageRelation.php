437916eabcea4357428a3acc5777e76fbb01eb98
zack <zb01406@gmail.com>
2022-10-09 20:22:44 -0400

    Added Damage Relation

--- app/Models/DamageRelation.php
+++ app/Models/DamageRelation.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class DamageRelation extends Pivot
 {
+    protected $guarded = [];
+    public $timestamps = false;
+
     protected $foreignKey = 'attacking_type_id';
     protected $relatedKey = 'defending_type_id';
     protected $table = 'damage_relation';
