1eb9d75fddcd03a01e8ae858185160c9d3684425
zack <zb01406@gmail.com>
2022-10-13 21:21:51 -0400

    Added Generation Seeder

---
+++ database/seeders/GenerationSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Jobs\ImportGeneration;
+use App\Models\Pokemon;
+use App\Models\Generation;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Http;
+use SmeltLabs\PocketMonsters\EndpointBuilder;
+
+class GenerationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 1. Truncate the table so you can run seeder by itself
+        Generation::truncate();
+
+        // 2. Fetch all the generations from the API
+        $api = new EndpointBuilder();
+        $generationJson = fetchJson($api->getAllGenerations());
+
+        // 3. Loop over a list of urls associated to each generation
+        $urls = collect($generationJson['results'])->pluck('url');
+        foreach ($urls as $generationUrl) {
+
+            // 4. Fetch all the data for a specific generation
+            $generationJson = fetchJson($generationUrl);
+
+            // 5. Assign variables for easy access.
+            $generationName = $generationJson['name'];
+            $pokemonNames = collect($generationJson['pokemon_species'])->pluck('name');
+
+            // 6. Loop over every pokemon name
+            foreach ($pokemonNames as $pokemonName) {
+                // 7. Find pokemon by unique pokemon name.
+
+                $pokemonDB = Pokemon::where('name', $pokemonName)->firstOrFail();
+
+                // 8. Dispatch ImportType job
+                ImportGeneration::dispatch($generationName, $pokemonDB);
+            }
+        }
+    }
+}
