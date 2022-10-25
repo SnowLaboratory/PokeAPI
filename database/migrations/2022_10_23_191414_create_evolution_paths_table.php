1d22235c075167cb59fcf55afa69d10a8199591e
zack <zb01406@gmail.com>
2022-10-24 20:36:46 -0400

    Modified EvolutionPath, Trigger, Trigger Requirements migrations

--- database/migrations/2022_10_23_191414_create_evolution_paths_table.php
+++ database/migrations/2022_10_23_191414_create_evolution_paths_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Trigger;
 
 return new class extends Migration
 {
@@ -15,6 +16,7 @@ return new class extends Migration
     {
         Schema::create('evolution_paths', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Trigger::class);
             $table->timestamps();
         });
     }
--- database/migrations/2022_10_23_191550_create_trigger_requirements_table.php
+++ database/migrations/2022_10_23_191550_create_trigger_requirements_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\EvolutionPath;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,7 +16,12 @@ return new class extends Migration
     {
         Schema::create('trigger_requirements', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name')->nullable();
+            $table->boolean('boolean_value')->nullable();
+            $table->integer('number_value')->nullable();
+            $table->dateTime('time_value')->nullable();
+            $table->foreignIdFor(EvolutionPath::class)->nullable();
+            $table->nullableMorphs('model');
         });
     }
 
