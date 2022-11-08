24c1f367041cb9877055e6831fdb8076d5d57d1f
zack <zb01406@gmail.com>
2022-11-07 22:07:13 -0500

    Added Sprite Seeder

--- database/seeders/SpriteSeeder.php
+++ database/seeders/SpriteSeeder.php
@@ -6,9 +6,11 @@ use App\Models\Sprite;
 use Database\Traits\CanDisplayProgress;
 use Database\Traits\CanTruncateTables;
 use Http;
+use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Storage;
 use SmeltLabs\PocketMonsters\EndpointBuilder;
+use App\Models\Pokemon;
 
 class SpriteSeeder extends Seeder
 {
@@ -32,22 +34,27 @@ class SpriteSeeder extends Seeder
         $this->progressMap($urls, function($pokemonUrl) {
 
             $pokemonJson = fetchJson($pokemonUrl);
-
+            $pokemonName = $pokemonJson['name'];
             $sprite = $pokemonJson['sprites'];
 
             $originalArtworkUrl = $sprite['other']['official-artwork']['front_default'];
+            $frontDefault = $sprite['front_default'];
+            $hash = sha1($originalArtworkUrl);
+            $fileName = $hash . '.png';
 
-            $imageData = Http::get($originalArtworkUrl)->body();
-            $hash = sha1($imageData);
-            $filename = $hash . '.png';
-            $image = Storage::disk('sprites')->put($filename, $imageData);
-            $storageUrl = Storage::disk('sprites')->url($filename);
-            dd($storageUrl);
+            $storageUrl = tap(Storage::disk('sprites'), function($disk) use($fileName, $originalArtworkUrl, $frontDefault) {
+                if(!$disk->exists($fileName)) {
+                    $imageData = Http::get($originalArtworkUrl ?? $frontDefault)->body();
+                    $disk->put($fileName, $imageData);
+                }
+            })->url($fileName);
 
-            // Sprite::firstOrCreate([
-            //     'name' => $pokemonJson['name'],
-            // ]);
-
+            $spriteDB = Sprite::firstOrCreate([
+                'storage_url' => $storageUrl,
+                'api_url' => $originalArtworkUrl
+            ]);
+            $pokemon = Pokemon::firstWhere('name', $pokemonName);
+            $spriteDB->pokemon()->associate($pokemon)->save();
         });
     }
 
