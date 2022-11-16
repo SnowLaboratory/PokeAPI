4650fd5ffd6abf01b9c6bb898ba364ab8433c149
Zeb <burroughszeb@me.com>
2022-11-16 17:12:26 -0600

    Add inertia SSR support (#48)
    * removes laravel readme
    
    * adds inertia and resource helper for inertia

---
+++ resources/js/Pages/Pokemon/Detail.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { usePage } from '@inertiajs/inertia-vue3';
+
+const species = usePage().props.value.species;
+console.log(species)
+</script>
+
+<template>
+    <h1>Hello <span class="font-bold">{{ species.name }}</span></h1>
+</template>
