13032f8b42839a4a4780ae89a5439e1f916c2d8c
Zeb <burroughszeb@me.com>
2022-12-04 23:10:29 -0600

    makes it possible to link nodes which move

--- resources/js/Components/Admin/Chain/Graph.vue
+++ resources/js/Components/Admin/Chain/Graph.vue
@@ -1,14 +1,19 @@
 <script setup>
 
-import {computed, onMounted, ref} from 'vue'
+import {computed, getCurrentInstance, onMounted, ref, watch} from 'vue'
 import interact from 'interactjs'
 import Draggable from './Draggable.vue';
 import Node from './Node.vue';
+import LeaderLine from 'leader-line-new'
 
 const emit = defineEmits([
     'move',
     'selected',
 ])
+
+// function defineRefs(refs) {
+//   return _.chain(refs).keyBy().mapValues(ref).value();
+// }
 
 const props = defineProps({
     edges: {
@@ -16,13 +21,13 @@ const props = defineProps({
         default: [
             {
                 data: {},
-                p1: {x: 20, y: 30, id:1},
-                p2: {x: 0, y: 0, id:2},
+                p1: {x: 20, y: 30, id:11},
+                p2: {x: 0, y: 0, id:21},
             },
             {
                 data: {},
-                p1: {x: 0, y: 0, id:1},
-                p2: {x: 0, y: 0, id:3},
+                p1: {x: 0, y: 0, id:11},
+                p2: {x: 0, y: 0, id:31},
             },
         ],
     }
@@ -30,13 +35,48 @@ const props = defineProps({
 
 const el = ref(null);
 
+const nodeRefs = ref([])
+
 const uniqueNodes = _.chain(props.edges)
     .map('p1')
     .concat(_.map(props.edges, 'p2'))
     .uniqBy('id')
+    .keyBy('id')
+    .mapValues(point => {
+        return {
+            position: {
+                x: point.x,
+                y: point.y,
+            },
+            id: point.id,
+            data: {},
+        }
+    })
     .value()
 
-const edges = ref(props.edges)
+    console.log(uniqueNodes)
+    // console.log(_.map(uniqueNodes, x => `point_${x.id}`))
+
+// const inputs = defineRefs(_.map(uniqueNodes, x => `point_${x.id}`))
+
+// const nodes = ref([])
+
+const edges = computed(() => props.edges)
+const updateEdges = () => {
+    edges.value.forEach(edge => {
+        // console.log()
+        const p1 = el.value.querySelector(`[data-id="${edge.p1.id}"]`);
+        const p2 = el.value.querySelector(`[data-id="${edge.p2.id}"]`);
+
+        // console.log(p1)
+        edge.line = edge.line ?? new LeaderLine(
+            p1, p2
+        )
+        edge.line.startPlugColor = getComputedStyle(p1).stroke
+        edge.line.endPlugColor = getComputedStyle(p2).stroke
+        edge.line.gradient = true
+    })
+}
 
 const handleSelect = () => {
     if (!props.locked) {
@@ -51,15 +91,35 @@ const bounds = computed(() => ({
     bottom: el.value?.getBoundingClientRect().bottom,
 }))
 
+const handleUpdate = (point) => {
+    edges.value.filter(edge => {
+        return edge.p1.id === point.id
+        || edge.p2.id === point.id
+    }).forEach(edge => {
+        edge.line.position()
+    })
+}
+
+onMounted(() => {
+    updateEdges()
+})
+
+watch(edges, (newValue) => {
+    if (newValue) updateEdges()
+})
+
 </script>
 
 <template>
     <div ref="el">
-        <template v-for="point in uniqueNodes">
-            <Node :bounds="bounds"
-                  :position="point"
-                  class="absolute h-6 w-6 bg-red-400 rounded-full stroke-blue-500"
+        <template v-for="(point,id) in uniqueNodes">
+                <Node :bounds="bounds"
+                  v-model:position="point.position"
+                  :data-id="point.id"
+                  @update:position="handleUpdate(point)"
+                  class="absolute h-6 w-6 bg-emerald-400 rounded-full stroke-slate-600"
                 />
+                <!-- {{ point.value.id }} -->
         </template>
         <slot></slot>
     </div>
--- resources/js/Components/Admin/Chain/Node.vue
+++ resources/js/Components/Admin/Chain/Node.vue
@@ -21,7 +21,7 @@ const props = defineProps({
     bounds: {
         type: Object,
         default: null,
-    }
+    },
 })
 
 
